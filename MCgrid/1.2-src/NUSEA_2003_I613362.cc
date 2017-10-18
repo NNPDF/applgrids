c1d5b4339d8ec17647283069c73102769cd8163d
Nathan Hartland <nathan.hartland@physics.ox.ac.uk>
2017-10-18 21:07:04 +0200

    mcgrid2 rearrangement

--- MCgrid/1.2-src/NUSEA_2003_I613362.cc
+++ MCgrid/1.2-src/NUSEA_2003_I613362.cc
@@ -30,17 +30,16 @@ namespace Rivet {
       addProjection(ChargedLeptons(clfs), "CL");
 
       // Initialise the subprocess PDF and grid architecture
-      MCgrid::applGridArch arch(80,40,3,3);
       const string PDFname("NUSEA_2003_I613362.config");
+      MCgrid::bookPDF(PDFname, histoDir(), MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON);
+      MCgrid::gridArch arch(80,40,3,3);
       const double xmin = 1E-2; const double Q2min = 4.2*4.2;
       const double xmax = 1;    const double Q2max = 16.85*16.85;
-      MCgrid::subprocessConfig subproc(PDFname, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON);
-      MCgrid::applGridConfig conf(0, subproc, arch, xmin, xmax, Q2min, Q2max);
 
       for (int i=0; i<bin_edge_xF.size()-1; i++)
       {
         Histo1DPtr      histogram = bookHisto1D(i+1, 1, 1);
-        MCgrid::gridPtr applgrid  = MCgrid::bookGrid(histogram, histoDir(), conf); 
+        MCgrid::gridPtr applgrid  = MCgrid::bookGrid(histogram, histoDir(), PDFname, 0, xmin, xmax, Q2min, Q2max, arch);
         std::cout << "Booking: " << bin_edge_xF[i] <<" - " << bin_edge_xF[i+1] <<std::endl;
         _hist_sigma.addHistogram(bin_edge_xF[i], bin_edge_xF[i+1], histogram);
         _appl_sigma.addGrid(bin_edge_xF[i], bin_edge_xF[i+1], applgrid);
@@ -59,10 +58,18 @@ namespace Rivet {
       const double M  = dilepton.mass();
       const double pZ = dilepton.pz();
       const double xF = 2.0*pZ / sqrts;
+
       if ( M > 8.7 && M < 10.85 ) vetoEvent;
-	
-      _hist_sigma.fill(xF, M, e.weight());
-      _appl_sigma.fill(xF, M, e);
+
+      // Scale weights by M**3
+      HepMC::GenEvent event_copy(*e.genEvent());
+      for (int i=0; i<event_copy.weights().size(); i++)
+        if (std::find(protect_wgts.begin(), protect_wgts.end(), i) == protect_wgts.end())
+          event_copy.weights()[i] *= pow(M,3.0)/nanobarn;
+      Event scaled_event(event_copy);
+
+      _hist_sigma.fill(xF, M, scaled_event.weight());
+      _appl_sigma.fill(xF, M, scaled_event);
     }
 
 
@@ -75,6 +82,7 @@ namespace Rivet {
 
 
   private:
+    const std::array<int, 4> protect_wgts = {{4,6,7,8}};
     const std::array<double, 17>  bin_edge_xF = {{-0.05,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8}};
     BinnedHistogram<double> _hist_sigma;
     MCgrid::BinnedGrid<double> _appl_sigma;
